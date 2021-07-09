0004fed6e303e97fd353520299c434d4ccfdba14
RajashekarRaju <rajashekarraju123@gmail.com>
2021-07-09 18:27:09 +0530

    Added all menu types, change of resources

--- app/src/main/java/com/example/menus_android/MainActivity.kt
+++ app/src/main/java/com/example/menus_android/MainActivity.kt
@@ -2,14 +2,12 @@ package com.example.menus_android
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.appcompat.widget.ListPopupWindow
+import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
@@ -24,38 +22,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         parent = findViewById(R.id.parent)
-        val showMenuTextView = findViewById<MaterialCardView>(R.id.popup_menu_card_view)
-        val showListPopupMenuTextView = findViewById<MaterialCardView>(R.id.list_popup_menu_card_view)
-        val showContextMenuTextView = findViewById<MaterialCardView>(R.id.context_menu_card_view)
+        val menuTextView = findViewById<MaterialCardView>(R.id.popup_menu_card_view)
+        val listPopupMenuTextView = findViewById<MaterialCardView>(R.id.list_popup_menu_card_view)
+        val contextMenuTextView = findViewById<MaterialCardView>(R.id.context_menu_card_view)
 
-        showContextMenuTextView.setOnLongClickListener {
-            showSnackbar("Add Context Options and remove this")
-            true
-        }
+        registerForContextMenu(contextMenuTextView)
 
         val textInputMenu = findViewById<TextInputLayout>(R.id.text_input_menu)
-        val items = listOf("Option 1", "Option 2", "Option 3", "Option 4")
-        val adapter = ArrayAdapter(applicationContext, R.layout.list_popup_window_item, items)
+        val items = listOf("Apparels", "Electronics", "Sports", "Kitchen")
+        val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, items)
         (textInputMenu.editText as? AutoCompleteTextView)?.setAdapter(adapter)
 
-        showMenuTextView.setOnClickListener { view ->
+        menuTextView.setOnClickListener { view ->
             showMenuOnClick(view)
         }
 
-        showListPopupMenuTextView.setOnClickListener { view ->
+        listPopupMenuTextView.setOnClickListener { view ->
             showListPopUpMenu(view)
         }
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_react_me, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_like -> showSnackbar("You liked it.")
+            R.id.menu_dislike -> showSnackbar("You disliked it.")
+        }
+        return super.onContextItemSelected(item)
     }
 
     @SuppressLint("RestrictedApi")
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.overflow_menu, menu)
-
         if (menu is MenuBuilder) {
             menu.setOptionalIconsVisible(true)
         }
-
         return true
     }
 
@@ -70,17 +81,20 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun showSnackbar(message: String) {
-        Snackbar.make(parent, message, Snackbar.LENGTH_SHORT).show()
-    }
-
     private fun showListPopUpMenu(view: View) {
         val listPopupWindow = ListPopupWindow(applicationContext, null, R.attr.listPopupWindowStyle)
         listPopupWindow.anchorView = view
 
         val items = listOf("One", "Two", "Three", "Four")
-        val adapter = ArrayAdapter(applicationContext, R.layout.list_popup_window_item, items)
+        val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, items)
         listPopupWindow.setAdapter(adapter)
+        listPopupWindow.setBackgroundDrawable(
+            ResourcesCompat.getDrawable(
+                resources,
+                R.color.list_bg,
+                applicationContext.theme
+            )
+        )
         listPopupWindow.show()
     }
 
@@ -110,5 +124,9 @@ class MainActivity : AppCompatActivity() {
 
             true
         }
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(parent, message, Snackbar.LENGTH_SHORT).show()
     }
 }
